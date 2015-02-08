c2b7be8d93408d6059f5c37119b9d941bb2e4dc2
unknown <jaclyncnguyen@utexas.edu>
2015-02-08 16:50:29 -0600

    craetion of files

---
+++ 01 Data/data_frame_ORCL.R
@@ -0,0 +1,1 @@
+df <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from emp"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_JCN565', PASS='orcl_jcn565', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
